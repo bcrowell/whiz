14bec08428080efb4fcafe2787054ff54bfa6090
Ben Crowell <notreal@fakewillnotwork.invalid>
2014-08-10 18:33:50 -0700

    self_service_hw_list basically working; see comments that say FIXME

--- whiz.rb
+++ whiz.rb
@@ -7,6 +7,7 @@
 #   whiz.rb points_possible '{"in_file":"hw.json","out_file":"points_possible.csv","book":"lm"}'
 #   whiz.rb sets_csv '{"in_file":"hw.json","out_file":"sets.csv","book":"lm","gb_file":"foo.gb"}'
 #          gb_file can be null string, for fake run with only Joe Blow on roster
+#   whiz.rb self_service_hw_list '{"in_file":"hw.json","out_file":"hw.html","book":"lm"}'
 #   optional args for sets_csv:
 #     class ... select only that class; error if some students don't have class set
 #   optional args for points_possible and hw_table:
@@ -443,6 +444,339 @@ def points_possible_to_csv(args)
   }
 end
 
+# http://stackoverflow.com/questions/1655769/fastest-md5-implementation-in-javascript
+# licensing unknown
+# tested as follows:
+# $ perl -e "use Digest::MD5 qw(md5_hex); print md5_hex('73')"
+# d2ddea18f00665ce8623e36bd4e3c7c5
+def myers_md5_implementation
+  return <<'JS'
+function md5cycle(x, k) {
+var a = x[0], b = x[1], c = x[2], d = x[3];
+
+a = ff(a, b, c, d, k[0], 7, -680876936);
+d = ff(d, a, b, c, k[1], 12, -389564586);
+c = ff(c, d, a, b, k[2], 17,  606105819);
+b = ff(b, c, d, a, k[3], 22, -1044525330);
+a = ff(a, b, c, d, k[4], 7, -176418897);
+d = ff(d, a, b, c, k[5], 12,  1200080426);
+c = ff(c, d, a, b, k[6], 17, -1473231341);
+b = ff(b, c, d, a, k[7], 22, -45705983);
+a = ff(a, b, c, d, k[8], 7,  1770035416);
+d = ff(d, a, b, c, k[9], 12, -1958414417);
+c = ff(c, d, a, b, k[10], 17, -42063);
+b = ff(b, c, d, a, k[11], 22, -1990404162);
+a = ff(a, b, c, d, k[12], 7,  1804603682);
+d = ff(d, a, b, c, k[13], 12, -40341101);
+c = ff(c, d, a, b, k[14], 17, -1502002290);
+b = ff(b, c, d, a, k[15], 22,  1236535329);
+
+a = gg(a, b, c, d, k[1], 5, -165796510);
+d = gg(d, a, b, c, k[6], 9, -1069501632);
+c = gg(c, d, a, b, k[11], 14,  643717713);
+b = gg(b, c, d, a, k[0], 20, -373897302);
+a = gg(a, b, c, d, k[5], 5, -701558691);
+d = gg(d, a, b, c, k[10], 9,  38016083);
+c = gg(c, d, a, b, k[15], 14, -660478335);
+b = gg(b, c, d, a, k[4], 20, -405537848);
+a = gg(a, b, c, d, k[9], 5,  568446438);
+d = gg(d, a, b, c, k[14], 9, -1019803690);
+c = gg(c, d, a, b, k[3], 14, -187363961);
+b = gg(b, c, d, a, k[8], 20,  1163531501);
+a = gg(a, b, c, d, k[13], 5, -1444681467);
+d = gg(d, a, b, c, k[2], 9, -51403784);
+c = gg(c, d, a, b, k[7], 14,  1735328473);
+b = gg(b, c, d, a, k[12], 20, -1926607734);
+
+a = hh(a, b, c, d, k[5], 4, -378558);
+d = hh(d, a, b, c, k[8], 11, -2022574463);
+c = hh(c, d, a, b, k[11], 16,  1839030562);
+b = hh(b, c, d, a, k[14], 23, -35309556);
+a = hh(a, b, c, d, k[1], 4, -1530992060);
+d = hh(d, a, b, c, k[4], 11,  1272893353);
+c = hh(c, d, a, b, k[7], 16, -155497632);
+b = hh(b, c, d, a, k[10], 23, -1094730640);
+a = hh(a, b, c, d, k[13], 4,  681279174);
+d = hh(d, a, b, c, k[0], 11, -358537222);
+c = hh(c, d, a, b, k[3], 16, -722521979);
+b = hh(b, c, d, a, k[6], 23,  76029189);
+a = hh(a, b, c, d, k[9], 4, -640364487);
+d = hh(d, a, b, c, k[12], 11, -421815835);
+c = hh(c, d, a, b, k[15], 16,  530742520);
+b = hh(b, c, d, a, k[2], 23, -995338651);
+
+a = ii(a, b, c, d, k[0], 6, -198630844);
+d = ii(d, a, b, c, k[7], 10,  1126891415);
+c = ii(c, d, a, b, k[14], 15, -1416354905);
+b = ii(b, c, d, a, k[5], 21, -57434055);
+a = ii(a, b, c, d, k[12], 6,  1700485571);
+d = ii(d, a, b, c, k[3], 10, -1894986606);
+c = ii(c, d, a, b, k[10], 15, -1051523);
+b = ii(b, c, d, a, k[1], 21, -2054922799);
+a = ii(a, b, c, d, k[8], 6,  1873313359);
+d = ii(d, a, b, c, k[15], 10, -30611744);
+c = ii(c, d, a, b, k[6], 15, -1560198380);
+b = ii(b, c, d, a, k[13], 21,  1309151649);
+a = ii(a, b, c, d, k[4], 6, -145523070);
+d = ii(d, a, b, c, k[11], 10, -1120210379);
+c = ii(c, d, a, b, k[2], 15,  718787259);
+b = ii(b, c, d, a, k[9], 21, -343485551);
+
+x[0] = add32(a, x[0]);
+x[1] = add32(b, x[1]);
+x[2] = add32(c, x[2]);
+x[3] = add32(d, x[3]);
+}
+
+function cmn(q, a, b, x, s, t) {
+a = add32(add32(a, q), add32(x, t));
+return add32((a << s) | (a >>> (32 - s)), b);
+}
+
+function ff(a, b, c, d, x, s, t) {
+return cmn((b & c) | ((~b) & d), a, b, x, s, t);
+}
+
+function gg(a, b, c, d, x, s, t) {
+return cmn((b & d) | (c & (~d)), a, b, x, s, t);
+}
+
+function hh(a, b, c, d, x, s, t) {
+return cmn(b ^ c ^ d, a, b, x, s, t);
+}
+
+function ii(a, b, c, d, x, s, t) {
+return cmn(c ^ (b | (~d)), a, b, x, s, t);
+}
+
+function md51(s) {
+txt = '';
+var n = s.length,
+state = [1732584193, -271733879, -1732584194, 271733878], i;
+for (i=64; i<=s.length; i+=64) {
+md5cycle(state, md5blk(s.substring(i-64, i)));
+}
+s = s.substring(i-64);
+var tail = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+for (i=0; i<s.length; i++)
+tail[i>>2] |= s.charCodeAt(i) << ((i%4) << 3);
+tail[i>>2] |= 0x80 << ((i%4) << 3);
+if (i > 55) {
+md5cycle(state, tail);
+for (i=0; i<16; i++) tail[i] = 0;
+}
+tail[14] = n*8;
+md5cycle(state, tail);
+return state;
+}
+
+/* there needs to be support for Unicode here,
+ * unless we pretend that we can redefine the MD-5
+ * algorithm for multi-byte characters (perhaps
+ * by adding every four 16-bit characters and
+ * shortening the sum to 32 bits). Otherwise
+ * I suggest performing MD-5 as if every character
+ * was two bytes--e.g., 0040 0025 = @%--but then
+ * how will an ordinary MD-5 sum be matched?
+ * There is no way to standardize text to something
+ * like UTF-8 before transformation; speed cost is
+ * utterly prohibitive. The JavaScript standard
+ * itself needs to look at this: it should start
+ * providing access to strings as preformed UTF-8
+ * 8-bit unsigned value arrays.
+ */
+function md5blk(s) { /* I figured global was faster.   */
+var md5blks = [], i; /* Andy King said do it this way. */
+for (i=0; i<64; i+=4) {
+md5blks[i>>2] = s.charCodeAt(i)
++ (s.charCodeAt(i+1) << 8)
++ (s.charCodeAt(i+2) << 16)
++ (s.charCodeAt(i+3) << 24);
+}
+return md5blks;
+}
+
+var hex_chr = '0123456789abcdef'.split('');
+
+function rhex(n)
+{
+var s='', j=0;
+for(; j<4; j++)
+s += hex_chr[(n >> (j * 8 + 4)) & 0x0F]
++ hex_chr[(n >> (j * 8)) & 0x0F];
+return s;
+}
+
+function hex(x) {
+for (var i=0; i<x.length; i++)
+x[i] = rhex(x[i]);
+return x.join('');
+}
+
+function md5(s) {
+return hex(md51(s));
+}
+
+/* this function is much faster,
+so if possible we use it. Some IEs
+are the only ones I know of that
+need the idiotic second function,
+generated by an if clause.  */
+
+function add32(a, b) {
+return (a + b) & 0xFFFFFFFF;
+}
+
+if (md5('hello') != '5d41402abc4b2a76b9719d911017c592') {
+function add32(x, y) {
+var lsw = (x & 0xFFFF) + (y & 0xFFFF),
+msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+return (msw << 16) | (lsw & 0xFFFF);
+}
+}
+JS
+end
+
+def self_service_hw_list(args)
+  unless args.has_key?('in_file') then fatal_error("args do not contain in_file key: #{JSON.generate(args)}") end
+  hw = get_json_data_from_file_or_die(args['in_file'])
+  unless args.has_key?('book') then fatal_error("args do not contain book key: #{JSON.generate(args)}") end
+  book = args['book']
+  sets = hw_to_sets(hw,book)
+
+  unless args.has_key?('out_file') then fatal_error("args do not contain out_file key: #{JSON.generate(args)}") end
+  title = "Homework Assignments"
+
+  d = [];
+  1.upto(sets.length-1) { |set_number|
+    set = sets[set_number]
+    d.push(['string',"<h2>Homework #{set_number}</h2>\n"]);
+    0.upto(1) { |online|
+      if online==0 then d.push(['string',"<h3>Online</h3>"]) else d.push(['string',"<h3>Paper</h3>"]) end
+      d.push(['string',"<p>"])
+      victims = []
+      set.each { |stream_group|
+        stream_group.each { |flag_group|
+          flags,probs = flag_group
+          is_online = (flags.has_key?("o"))
+          if (is_online && online==1) || (!is_online && online==0) then
+            probs.each { |individualization_group|
+              victims.push([flags,individualization_group])
+            }
+          end
+        }
+      } # end loop over stream groups
+      stuff = []
+      victims.sort {|u,v| spaceship_individualization_group(u[1],v[1])}.each { |v|
+        flags,individualization_group = v
+        if individualization_group.length==1 then
+          stuff.push(describe_individualization_group(flags,individualization_group,'html'))
+        else
+          chapter = individualization_group[0][0]
+          pp = []
+          description = []
+          individualization_group.each { |p|
+            pp.push(p[1])
+            description.push(p[0].to_s+"-"+describe_prob_and_parts(p)) # FIXME -- won't display flags
+          }
+          year = '2014' # FIXME
+          semester = 'f'; # FIXME
+          stuff.push({"chapter"=>chapter,"year"=>year,"semester"=>semester,"probs"=>pp,
+                        "description"=>description})
+        end
+      }
+      if stuff.length==0 then 
+        d.push(['string','none'])
+      else
+        stuff.each { |x|
+          if x.class.to_s=="String" then
+            d.push(['string',x+" "])
+          else
+            d.push(['indiv',x])
+          end
+        }
+      end
+      d.push(['string',"</p>"])
+    } # end loop over paper and online
+  } # end loop over sets
+  
+  js = <<-"JS"
+    #{myers_md5_implementation}
+    function the_hw(student_id) {
+      hw_data = #{JSON.generate(d)};
+      var html = "";
+      for (var i=0; i<hw_data.length; i++) {
+        t = hw_data[i][0];
+        x = hw_data[i][1];
+        if (t==='string') { html = html + x; }
+        if (t==='indiv') {
+          chapter = x["chapter"];
+          year = x["year"];
+          semester = x["semester"];
+          probs = x["probs"];
+          description = x["description"];
+          var m = select_using_hash(chapter,probs,year,semester,student_id);
+          //html = html + "m="+String(m)+" ";
+          html = html + description[m]+" ";
+        }
+      }
+      return html;
+    }
+    // returns a number from 0 to n-1, where n is the length of the array probs
+    function select_using_hash(chapter,probs,year,semester,student_id) {
+      // year = 2014, etc.
+      // semester = "s" or "f"
+      chapter = String(chapter);
+      year = Number(year); // integer such as 2014
+      semester = String(semester);
+      student_id = String(student_id);
+      // figure out an integer for the semester, counting from spring 2014 = 0
+      var s = (year-2014)*2;
+      if (semester==="f") {s=s+1;}
+      pp = probs.slice(0); // clone it; http://stackoverflow.com/questions/597588/how-do-you-clone-an-array-of-objects-in-javascript
+      pp.sort(); // changes the array
+      var hash = md5(student_id+","+chapter+","+pp.join(":")); // 32 hex digits
+      hash = hash.substring(28,31);
+      k = parseInt(hash,16);
+      k = k+s; // if a student is repeating the course, cycle through the problems, don't assign same one
+      n = probs.length;
+      return k%n;
+    }
+    function calculate() {
+      var p = document.getElementById("output");
+      var student_id = document.getElementById("student_id").value;
+      var result = "";
+      result = result + "<h1>Your Homework</h2><p>This was generated for student ID "+student_id+". Please print it out.</p>"
+          +"<p>The MD5 of your student ID is "+md5(student_id)+".</p>";
+      result = result + the_hw(student_id);
+      p.innerHTML = result;
+    }
+    function clear_output() {
+      document.getElementById("output").innerHTML="";
+    }
+    JS
+  html = <<-"HTML"
+    <!doctype html>
+    <html lang="en">
+      <head><meta charset="utf-8"><title>#{title}</title></head>
+      <body>
+        <form name="myform">
+          <p>
+            Enter your student ID:
+                   <input type="text" name="student_id" id="student_id">
+            <input type="button" value="Enter" onclick="calculate()">
+          </p>
+        </form>
+        <p id="output"></p>
+        <script>#{js}</script>
+           </body>
+    </html>
+    HTML
+  File.open(args['out_file'],'w') { |f|
+    f.print html
+  }
+end
+
 ################################################################################################
 
 
@@ -460,4 +794,5 @@ if $verb=="parse_hw" then parse_hw($args); exit(0) end
 if $verb=="hw_table" then hw_table($args); exit(0) end
 if $verb=="points_possible" then points_possible_to_csv($args); exit(0) end
 if $verb=="sets_csv" then sets_csv($args); exit(0) end
+if $verb=="self_service_hw_list" then self_service_hw_list($args); exit(0) end
 fatal_error("unrecognized verb: $verb")
